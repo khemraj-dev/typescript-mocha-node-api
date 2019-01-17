c027b0368c491d81b7648b9426fe8fcf923d4b2d
khemraj-dev <khemraj.47billion@gmail.com>
2019-01-17 21:28:44 +0530

    GET hero routes

--- src/App.ts
+++ src/App.ts
@@ -3,6 +3,9 @@ import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 
+import HeroRouter from './routes/HeroRouter';
+
+
 // Creates and configures an ExpressJS web server.
 class App {
 
@@ -23,7 +26,6 @@ class App {
         this.express.use(bodyParser.urlencoded({ extended: false }));
     }
 
-    // Configure API endpoints.
     private routes(): void {
         /* This is just to get up and running, and to make sure what we've got is
          * working so far. This function will change when we start to add more
@@ -36,6 +38,7 @@ class App {
             });
         });
         this.express.use('/', router);
+        this.express.use('/api/v1/heroes', HeroRouter);
     }
 
 }
